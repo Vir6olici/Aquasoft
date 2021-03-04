0a91da98b33f62c84b60f1046c6bc662f2fcca9b
Vir6olici <68445529+Vir6olici@users.noreply.github.com>
2021-03-04 10:41:33 +0200

    Add files via upload

---
+++ react/src/messages.js
@@ -0,0 +1,10 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+
+const messages = () => {
+    return(
+        <div>Aici o sa pun mesajele!</div>
+    )
+}
+
+export default messages;
